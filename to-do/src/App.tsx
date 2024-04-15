7af248f258114121a64df413efa370a1c46378a4
RennI <98748434+Renne103@users.noreply.github.com>
2024-04-15 16:29:34 +0300

    refactor code

--- to-do/src/App.tsx
+++ to-do/src/App.tsx
@@ -18,6 +18,12 @@ function App() {
     setTasks(filteredTasks);
   }
 
+  function addTask(title: string) {
+    let newTask = { id: v1(), title: title, isDone: false };
+    let newTasks = [newTask, ...tasks];
+    setTasks(newTasks);
+  }
+
   function changeFilter(value: FilterValuesType) {
     setFilter(value);
   }
@@ -38,6 +44,7 @@ function App() {
         tasks={tasksForTodolist}
         removeTask={removeTask}
         changeFilter={changeFilter}
+        addTask={addTask}
       />
     </div>
   );
